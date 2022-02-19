9663755ba11f1cbd4f4d5f778ad9171074815d65
Indi Rinearson <indi.rinearson@gmail.com>
2022-02-18 19:23:40 -0800

    chore: mounted navbar in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
         <Home/>
-        {/* ultimately navbar and routes go here */}
+        {/* ultimately routes go here */}
     </div>
   );
 }
