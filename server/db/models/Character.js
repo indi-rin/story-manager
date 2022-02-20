13c7efedf8624d1806f8f67265cc137b7114b14d
Indi Rinearson <indi.rinearson@gmail.com>
2022-02-20 14:11:57 -0800

    character model

--- server/db/models/Character.js
+++ server/db/models/Character.js
@@ -1,6 +1,25 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
 
-const Character = db.define("character", {});
+const Character = db.define("character", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  fullName: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  bio: {
+    type: Sequelize.STRING,
+  },
+  notes: {
+    type: Sequelize.TEXT,
+  },
+});
+
+// any methods or hooks?
 
 module.exports = Character;
