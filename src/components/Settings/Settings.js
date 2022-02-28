cad6a69a9e051f877ec2c3708cb3bdf75bfc3cf5
Indi Rinearson <indi.rinearson@gmail.com>
2022-02-27 17:57:13 -0800

    test: users api

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsers } from "../../store/reducers/users"; // redux --> api route for testing purposes
 
 const Settings = () => {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
+
   return (
     <>
       <h1>Settings!</h1>
+      <p>First User: {users[0]}</p>
     </>
   );
 };
